1e322999980b878e78c3c153955474f1fc0dc89f
ErielVasan <35623989+ErielVasan@users.noreply.github.com>
2021-02-08 17:51:06 +0200

    Bugfix
    Fixed the bug when pressing back from missions activity to act like home and not open or close any other activities

--- app/src/main/java/com/example/dailymissions/MissionsActivity.kt
+++ app/src/main/java/com/example/dailymissions/MissionsActivity.kt
@@ -2,6 +2,7 @@ package com.example.dailymissions
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
@@ -13,14 +14,20 @@ class MissionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_missions)
+
+        findViewById<Button>(R.id.signOutButton).setOnClickListener {
+            firebaseAuth.signOut()
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 
+
     override fun onBackPressed() {
-        super.onBackPressed()
         val intent = Intent()
         intent.action = Intent.ACTION_MAIN
         intent.addCategory(Intent.CATEGORY_HOME)
         startActivity(intent)
-
     }
 }
